ba81404c622d5195e6f1e8004440f27d7d3a2186
Ajit Gupta <57554561+devajitgupta@users.noreply.github.com>
2020-03-17 18:41:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const userRoutes=require("./routes/user");
 
 
 //connect to database
-mongoose.connect(process.env.mongodb://ajitgupta:ajitgupta@cluster0-shard-00-00-smarh.mongodb.net:27017,cluster0-shard-00-01-smarh.mongodb.net:27017,cluster0-shard-00-02-smarh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority,{ useNewUrlParser: true,useUnifiedTopology: false},()=>
+mongoose.connect(process.env.'mongodb://ajitgupta:ajitgupta@cluster0-shard-00-00-smarh.mongodb.net:27017,cluster0-shard-00-01-smarh.mongodb.net:27017,cluster0-shard-00-02-smarh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: false},()=>
 	console.log("connected to DB"));
 //====== middleware
 app.use(bodyParser.urlencoded({extended:true}));
